7f423874401696f4fff140df7a6a0bcb82dd8d87
Lu1sR0 <luiscardoso946@mail.com>
2024-08-03 17:09:04 -0300

    first commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,22 @@
+import { Environment, Float, OrbitControls } from "@react-three/drei";
+import { Book } from "./Book";
+export const Experience = () => {
+  return (
+    <>
+      <Float
+        rotation-x={-Math.PI / 4}
+        floatIntensity={1}
+        speed={2}
+        rotationIntensity={2}
+      >
+        <Book />
+      </Float>
+      <OrbitControls />
+      <Environment preset="studio"></Environment>
+      <mesh position-y={-1.5} rotation-x={-Math.PI / 2} receiveShadow>
+        <planeGeometry args={[100, 100]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
+    </>
+  );
+};
